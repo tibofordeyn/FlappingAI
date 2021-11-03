5ea37ffe5767aa58f3c26001059ccf584afd6874
TiboFord <tibofordeyninsta@gmail.com>
2021-11-04 00:29:20 +0100

    Just rewrote a lot of the code object oriented. Costed me a rather large amount of time as a beginner. Updated the readme file

--- README.md
+++ README.md
@@ -4,8 +4,10 @@
 __This is my first programming project in which I will be making a clone of the flappy bird game, and an AI that will be able to play it.__
 
 <!--MAIN INFO-->
-## Main info
+## Current updates
 At the time of writing, I just started this project. Therefore I haven't really got anything left to say right now. This file will be updated regularly throughout the projects evolution.
+
+I am right now thinking about wether to make a moving floor which you aren't allowed to touch or not. I personally think it's uselss and ugly, however I've never seen flappy bird without a surface.
 
 <!--HOW TO RUN THE CODE-->
 ## Running my program
--- main.py
+++ main.py
@@ -8,11 +8,12 @@ I now realise I'll have to write this code using 3 classes:
     and 1 more for the floor
 '''
 
+
 import pygame as pg 
 from random import randint as rd 
 from os import sys
-import neat
-import time
+import neat as nt
+import time as tm
 pg.init()
 
 
@@ -20,6 +21,7 @@ pg.init()
 SCREENWIDTH = 864
 SCREENHEIGHT = 1200
 SCREEN = pg.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+pg.display.set_caption("AI  PLAYS  FLAPPY  BIRD !")
 
 
 #   STATIC INITIALS - CLOCK
@@ -28,18 +30,24 @@ FPS = 120
 FRAMESAM = 0
 
 
+#   STATIC INITIALS - MOVEMENT
+BIRDJUMP = -200
+
+
 #   STATIC INITIALS - BACKGROUNDS + BACKGROUND DISPLAY
 BACKGROUNDS = (
         pg.image.load("images/backgrounds/background-day.png").convert(),
         pg.image.load("images/backgrounds/background-night.png").convert(),
         pg.image.load("images/backgrounds/MarioBackground-static.png").convert(),
         pg.image.load("images/backgrounds/spacebg.png").convert(),
+        pg.image.load("images/backgrounds/underwater.jpeg").convert(),
 )
 SCALED_BACKGROUNDS = (
     pg.transform.scale(BACKGROUNDS[0],(SCREENWIDTH, SCREENHEIGHT)),
     pg.transform.scale(BACKGROUNDS[1],(SCREENWIDTH, SCREENHEIGHT)),
     pg.transform.scale(BACKGROUNDS[2],(SCREENWIDTH, SCREENHEIGHT)),
     pg.transform.scale(BACKGROUNDS[3],(SCREENWIDTH, SCREENHEIGHT)),
+    pg.transform.scale(BACKGROUNDS[4],(SCREENWIDTH, SCREENHEIGHT)),
 )
 BAHIGHEST_INDEX = len(BACKGROUNDS)-1
 RANDBACK = rd(0,BAHIGHEST_INDEX)
@@ -54,9 +62,7 @@ every 5 points.
 DOWNFLAP = 0
 MIDFLAP = 1
 UPFLAP = 2
-
 FLAPINDEX = 0
-
 BIRDPOS = (
     SCREENWIDTH/4,
     SCREENHEIGHT/2,
@@ -65,7 +71,6 @@ BIRDSSIZE = (
     int(SCREENWIDTH/10),
     int(SCREENHEIGHT/20),
 )
-
 BIRDS = (
     (
         pg.image.load("images/birds/bluebird-downflap.png").convert(),
@@ -114,17 +119,17 @@ HIGHEST_BINDEX is the highest index in the birds list.
 RANDBIRD will be used to randomly choose which bird to display.
 """
 
+
 #   STATIC INITIALS - THE FLOOR
 FLOORSIZE = (
     int(SCREENWIDTH),
     int(SCREENHEIGHT/5),
 )
-
 FLOORS = (
     pg.image.load("images/floors/base.png").convert(),
 )
 SCALED_FLOORS = (
-    pg.transform.scale(FLOORSURFACE[0],(FLOORSIZE[0],FLOORSIZE[1])),
+    pg.transform.scale(FLOORS[0],(FLOORSIZE[0],FLOORSIZE[1])),
 )
 RECT_FLOORS = (
     SCALED_FLOORS[0].get_rect(left = 0, right = SCREENHEIGHT)
@@ -137,3 +142,97 @@ every 5 points. It will change at the same time as the
 background.
 """
 
+
+#   CREATING THE CLASSES - BIRDS
+class Bird:
+
+    DOWNFLAP = 0
+    MIDFLAP = 1
+    UPFLAP = 2
+    FLAPINDEX = 0
+
+    def __init__(self,x,y):
+        """
+        Initial values that will be used in the other functions
+        """
+        self.x = x
+        self.y = y
+        self.tickc = 0
+        self.grav = 0
+        self.height = self.y
+        self.imgcount = 0
+        self.imgs = SCALED_BIRDS[RANDBIRD][self.FLAPINDEX]
+
+    def jump(self):
+        self.grav = BIRDJUMP
+        self.tickc = 0
+        self.height = self.y
+
+    def move(self):
+        self.tickc +=1
+
+        DISPLACEMENT = self.grav*self.tickc + 1.5*self.tickc**2
+        if DISPLACEMENT >= 16:
+            DISPLACEMENT = 16
+        elif DISPLACEMENT < 0:
+            DISPLACEMENT -=4
+        self.y += DISPLACEMENT
+
+    def flapping(self, screen):
+        """
+        This function will change the displayed image
+        so that the bird flaps, except for when it is going
+        down.
+        """
+        self.imgcount += 1
+        self.imgs = SCALED_BIRDS[RANDBIRD][self.FLAPINDEX]
+        DISPLACEMENT = self.grav*self.tickc + 1.5*self.tickc**2
+
+        if self.imgcount%50==0:
+            if self.FLAPINDEX < self.UPFLAP:
+                self.FLAPINDEX += 1
+            else:
+                self.FLAPINDEX -= self.FLAPINDEX
+        if DISPLACEMENT < 0:
+            self.FLAPINDEX = 1
+
+        screen.blit(self.imgs, (self.x,self.y))
+
+    def get_prop(self):
+        return pg.mask.from_surface(self.imgs)
+    
+
+#   PROMINENT FUNCTIONS - DRAWING THE WINDOW
+def make_screen(screen, bird):
+    """
+    Draws stuff on the screen
+    First line is for blitting the background
+    """
+    screen.blit(SCALED_BACKGROUNDS[RANDBACK],(0,0))
+
+    bird.flapping(screen)
+
+    pg.display.update()
+
+
+#   PROMINENT FUNCTIONS - MAIN FUNCTION
+def main():
+    bird1 = Bird(BIRDPOS[0],BIRDPOS[1])
+    SCREEN = pg.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_d) or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+
+        make_screen(SCREEN, bird1)
+
+
+
+
+
+
+
+
+main()
